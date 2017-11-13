f27620a2a58d277c4b7f64cab74dac8e1cf6427b
Islamame <31524617+Islamame@users.noreply.github.com>
2017-11-13 16:50:33 +0200

    firstCommit
    the first

---
+++ Assets/Teest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Teest : MonoBehaviour {
+
+	public Text t;
+
+	public void Display(){
+		t.text = "AMIR";
+	}
+}
